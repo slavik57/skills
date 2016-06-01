59e1e452e9efb37cfa72d746dac51098e32c2538
Slava Shpitalny <slavik57@gmail.com>
2016-06-01 21:31:28 +0300

    Add knex and user table

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,6 @@
+/// <reference path="globals/knex/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
 /// <reference path="modules/chai/index.d.ts" />
 /// <reference path="modules/sinon/index.d.ts" />
