02d85c3ab3234771a616ffdb53beb847b047dd7d
Slava Shpitalny <slavik57@gmail.com>
2016-06-12 13:37:50 +0300

    Add password-hash npm module and typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,6 +4,7 @@
 /// <reference path="globals/knex/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/password-hash/index.d.ts" />
 /// <reference path="modules/bluebird/index.d.ts" />
 /// <reference path="modules/chai-as-promised/index.d.ts" />
 /// <reference path="modules/chai/index.d.ts" />
