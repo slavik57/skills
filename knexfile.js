ac902f8da17c5676dad97af81adfa5071db3950c
Slava Shpitalny <slavik57@gmail.com>
2016-06-13 23:20:25 +0300

    Update the knexfile

--- knexfile.js
+++ knexfile.js
@@ -12,8 +12,30 @@ var config = {
         migrations: {
             tableName: 'knex_migrations'
         }
+    },
+    development: {
+        client: 'postgresql',
+        connection: {
+            database: EnvironmentConfig.development.databbaseConfig.databaseName,
+            user: EnvironmentConfig.development.databbaseConfig.databaseUsername,
+            password: EnvironmentConfig.development.databbaseConfig.databasePassword,
+            host: EnvironmentConfig.development.databbaseConfig.databaseHost
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
+    },
+    tests: {
+        client: 'postgresql',
+        connection: {
+            database: EnvironmentConfig.tests.databbaseConfig.databaseName,
+            user: EnvironmentConfig.tests.databbaseConfig.databaseUsername,
+            password: EnvironmentConfig.tests.databbaseConfig.databasePassword,
+            host: EnvironmentConfig.tests.databbaseConfig.databaseHost
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     }
 };
-config['development'] = config.knexConfig;
-config['tests'] = config.knexConfig;
 module.exports = config;
