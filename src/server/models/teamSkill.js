a86261958dd81ca390d99eb30f58e826b8a30d0b
Slava Shpitalny <slavik57@gmail.com>
2016-06-17 20:55:19 +0300

    move to client and server folders

---
+++ src/server/models/teamSkill.js
@@ -0,0 +1,98 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var modelBase_1 = require("./modelBase");
+var skill_1 = require("./skill");
+var team_1 = require("./team");
+var teamSkillUpvote_1 = require("./teamSkillUpvote");
+var bookshelf_1 = require('../../../bookshelf');
+var Promise = require('bluebird');
+var typesValidator_1 = require('../../common/typesValidator');
+var TeamSkill = (function (_super) {
+    __extends(TeamSkill, _super);
+    function TeamSkill() {
+        _super.apply(this, arguments);
+    }
+    Object.defineProperty(TeamSkill.prototype, "tableName", {
+        get: function () { return 'team_skills'; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TeamSkill, "dependents", {
+        get: function () {
+            return [
+                TeamSkill.relatedTeamSkillUpvotesAttribute
+            ];
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TeamSkill, "skillIdAttribute", {
+        get: function () { return 'skill_id'; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TeamSkill, "teamIdAttribute", {
+        get: function () { return 'team_id'; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TeamSkill, "relatedTeamSkillUpvotesAttribute", {
+        get: function () { return 'upvotes'; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TeamSkill, "relatedTeamAttribute", {
+        get: function () { return 'team'; },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(TeamSkill, "relatedSkillAttribute", {
+        get: function () { return 'skill'; },
+        enumerable: true,
+        configurable: true
+    });
+    TeamSkill.collection = function (teamSkills, options) {
+        return new TeamSkills(teamSkills, options);
+    };
+    TeamSkill.prototype.initialize = function () {
+        var _this = this;
+        this.on('saving', function (teamSkill) { return _this._validateTeamSkill(teamSkill); });
+    };
+    TeamSkill.prototype.upvotes = function () {
+        return this.hasMany(teamSkillUpvote_1.TeamSkillUpvote, teamSkillUpvote_1.TeamSkillUpvote.teamSkillIdAttribute);
+    };
+    TeamSkill.prototype.team = function () {
+        return this.belongsTo(team_1.Team, TeamSkill.teamIdAttribute);
+    };
+    TeamSkill.prototype.skill = function () {
+        return this.belongsTo(skill_1.Skill, TeamSkill.skillIdAttribute);
+    };
+    TeamSkill.prototype._validateTeamSkill = function (teamSkill) {
+        if (!typesValidator_1.TypesValidator.isInteger(teamSkill.attributes.team_id)) {
+            return Promise.reject('The team_id must be an integer');
+        }
+        if (!typesValidator_1.TypesValidator.isInteger(teamSkill.attributes.skill_id)) {
+            return Promise.reject('The skill_id be an integer');
+        }
+        return Promise.resolve(true);
+    };
+    return TeamSkill;
+}(modelBase_1.ModelBase));
+exports.TeamSkill = TeamSkill;
+var TeamSkills = (function (_super) {
+    __extends(TeamSkills, _super);
+    function TeamSkills() {
+        _super.apply(this, arguments);
+        this.model = TeamSkill;
+    }
+    TeamSkills.clearAll = function () {
+        return new TeamSkills().query().del();
+    };
+    return TeamSkills;
+}(bookshelf_1.bookshelf.Collection));
+exports.TeamSkills = TeamSkills;
+//# sourceMappingURL=teamSkill.js.map
---
+++ src/server/models/teamSkillUpvote.ts
@@ -0,0 +1,25 @@
+import {ModelBase} from "./modelBase";
+import {Model, Collection, EventFunction, CollectionOptions} from 'bookshelf';
+import {bookshelf} from '../../../bookshelf';
+import * as Promise from 'bluebird';
+import {TypesValidator} from '../../common/typesValidator';
+import {ITeamSkillUpvoteInfo} from './interfaces/iTeamSkillUpvoteInfo';
+
+export class TeamSkillUpvote extends ModelBase<TeamSkillUpvote, ITeamSkillUpvoteInfo>{
+  public get tableName(): string { return 'team_skill_upvotes'; }
+
+  public static get teamSkillIdAttribute(): string { return 'team_skill_id' }
+  public static get userIdAttribute(): string { return 'user_id' }
+
+  public static collection(upvotes?: TeamSkillUpvote[], options?: CollectionOptions<TeamSkillUpvote>): Collection<TeamSkillUpvote> {
+    return new TeamSkillUpvotes(upvotes, options);
+  }
+}
+
+export class TeamSkillUpvotes extends bookshelf.Collection<TeamSkillUpvote> {
+  model = TeamSkillUpvote;
+
+  public static clearAll(): Promise<any> {
+    return new TeamSkillUpvotes().query().del();
+  }
+}
